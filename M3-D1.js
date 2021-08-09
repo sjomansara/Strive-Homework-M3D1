390288f5a4f81aa6ce035226b7cbf73958f160e2
sjomansara <sara.sjoman@hotmail.com>
2021-08-09 16:38:18 +0200

    solved ex12

--- M3-D1.js
+++ M3-D1.js
@@ -162,8 +162,24 @@ Create a function to find the types of a given angle.
 Types of angles:
     Acute angle: An angle between 0 and 90 degrees.
     Right angle: An 90 degree angle.
-    btuse angle: An angle between 90 and 180 degrees.
+    Obtuse angle: An angle between 90 and 180 degrees.
     Straight angle: A 180 degree angle.
+*/
+
+function judgeAngle(angle) {
+    if (angle > 0 && angle < 90) {
+        return "Acute angle"
+    } else if (angle === 90) {
+        return "Right angle"
+    } else if (angle > 90 && angle < 180) {
+        return "Obtuse angle"
+    } else if (angle === 180) {
+        return "Straight line"
+    }
+}
+console.log(judgeAngle(180))
+
+/*
 
 13)
 
